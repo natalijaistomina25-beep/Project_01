4b288f4c4764798df47acb16d92e353fb444b63b
Natalija <natalijaistomina25@gmail.com>
2026-01-12 19:35:19 +0100

    update NationalityReveal

--- src/components/NationalityReveal/NationalityReveal.tsx
+++ src/components/NationalityReveal/NationalityReveal.tsx
@@ -1,49 +1,60 @@
 
 import { useState } from "react";
+import type { NationalityInformation } from "../../types/NationalityInformation";
 import s from "./NationalityReveal.module.css";
 import { Results } from "./Results";
 
-
-type CountryInfo = {
-  country_id: string;
-  probability: number;
-};
-
-type NationalityInformation = {
-  name: string;
-  country: CountryInfo[];
-};
-
-
 export default function NationalityReveal() {
-   const [name, setName] = useState<string>("");
-   const [info, setInfo] = useState<NationalityInformation | undefined>(undefined);
+  const [name, setName] = useState<string>("");
+  const [info, setInfo] = useState<NationalityInformation | undefined>(undefined);
 
-    async function fetchNationality(name: string) {
-    if (!name.trim()) return; 
+  
+  const countryNames: { [key: string]: string } = {
+    US: "United States",
+    IE: "Ireland",
+    LV: "Latvia",
+    RU: "Russian Federation",
+    IT:"Italy",
+    ES:"Spain",
+  };
+
+  async function fetchNationality(name: string) {
+    
+    if (!name.trim()) {
+      alert("Please enter a valid name");
+      setInfo(undefined);
+      return;
+    }
+
     const res = await fetch(`https://api.nationalize.io/?name=${name}`);
-    const data: NationalityInformation = await res.json();
-    setInfo(data); 
+    const data = await res.json();
+    
+    // Преобразуем страны в человекочитаемый формат
+    const transformedData = {
+      ...data,
+      country: data.country.map((c: any) => ({
+        ...c,
+        country_name: countryNames[c.country_id] || c.country_id,
+      })),
+    };
+    setInfo(transformedData);
   }
 
   return (
     <div className={s.container}>
       <h2>Nationality Reveal</h2>
-
-      
       <input
         type="text"
         value={name}
         onChange={(e) => setName(e.target.value)}
         placeholder="Your Name"
       />
-
-      
-      <button type="button" onClick={() => fetchNationality(name)}>
+      <button
+        type="button"
+        onClick={() => fetchNationality(name)}
+      >
         Submit
       </button>
-
-      
       <Results info={info} />
     </div>
   );
